fc323b97b65b4418aa0cbc98bc4e4f49e9eb7455
Robert Osipovitch <90695804+RobOsipo@users.noreply.github.com>
2022-03-16 10:06:43 -0400

    create state + toggle light/dark

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-
+import Navbar from "./Navbar.js";
+import React from "react"
+import Main from "./Main.js";
 import './App.css';
 
 function App() {
+
+  const [darkMode, setDarkMode] = React.useState(false)
+
+
+    function toggle() {
+        setDarkMode(prevState => !prevState)
+    }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Hello hope I see this!</h1>
-      </header>
-    </div>
+  
+      <div className="container">
+        <Navbar
+            toggleDarkMode={toggle}
+             darkMode={darkMode} 
+             />
+          
+          <Main 
+            
+            darkMode={darkMode} 
+            />
+
+      </div>
+     
   );
 }
 
